9ea8cfe480e5669db9a5f98bc5fb46f5e65392fc
Khurshida-Meem <meemrumi59@gmail.com>
2022-04-05 07:23:38 +0600

    updated all visible thing

---
+++ src/Components/ExpenseItem/Expense.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Expense.css'
+import Card from './Card';
+import ExpensesFilter from './ExpensesFilter';
+import ExpenseList from './ExpenseList';
+
+const Expense = (props) => {
+
+  const items = props.items;
+  const [filteredYear, setFilteredYear] = useState('2022');
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+    <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      
+    <ExpenseList items={filteredExpenses} />
+    </Card>
+  );
+};
+
+export default Expense;
